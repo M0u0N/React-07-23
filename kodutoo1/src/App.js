bc4d65793fc48a1c4ab30fa565eefdcf6ef86aaf
M0u0N <140327848+M0u0N@users.noreply.github.com>
2023-07-24 14:24:41 +0300

    kodutoo

--- kodutoo1/src/App.js
+++ kodutoo1/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Link to="nupp">
+        <button className='nupp'>nupp</button>
+      </Link>
+
+      <Link to="text">
+        <text className='text'> sinine tekst muudab suurust </text>
+      </Link>
+
+      <Link to="tekst">
+        <text className='tekst'>roheline tekst</text>
+      </Link>
+
+      <Link to="pilt">
+        <img className="pilt" src="https://media.wired.com/photos/5a5547032b3a7778f5ca06cb/1:1/w_1200,h_1200,c_limit/Doggo-FeatureArt2-104685145.jpg" alt="" />
+      </Link>
     </div>
   );
 }
